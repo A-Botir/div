e81c4db06c7358b3cfeb75abbc7e74d45d0f55ed
A_Botir <shirokirutsuga>
2024-02-16 04:12:42 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App(title, firstbtn) {
       <div className='main_container'>
         <div className='centre_block h-40 bg-white flex justify-center items-center gap-5'>
           <ul className='block_list'>
-            <li className='list_item'><span className='list_item__num'>1</span>Choose title</li>
-            <li className='list_item'><span className='list_item__num'>2</span>Choose description</li>
-            <li className='list_item'><span className='list_item__num'>3</span>Confirm data</li>
+            <li><button className='list_item'><span className='list_item__num'>1</span>Choose title</button></li>
+            <li><button className='list_item'><span className='list_item__num'>2</span>Choose description</button></li>
+            <li><button className='list_item'><span className='list_item__num'>3</span>Confirm data</button></li>
           </ul>
           <div>
             <h3 className='block_title'></h3>
